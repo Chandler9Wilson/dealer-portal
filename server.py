c812508e377a56452b06ab972cdad510040c0c33
Chandler Wilson <chandler9wilson@gmail.com>
2017-11-20 14:57:38 -0600

    each session has a unique token

--- server.py
+++ server.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from flask import session as login_session
+import random
+import string
+
 app = Flask(__name__)
 
+
 @app.route("/")
-@app.route("/directory")
-def HelloWorld():
-    return 'Hello World'
+def login():
+    # TODO change route to /login
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    return "The current session state is %s" % login_session['state']
+
 
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
