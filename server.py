66da8a4c1e9056802bcc75b550bd7a03f2b7d879
Chandler Wilson <chandler9wilson@gmail.com>
2017-11-27 17:58:50 -0600

    removed jquery, added signInCallback using fetch (not entirely sure it will work yet), need to add /gconnect to servery.py

--- server.py
+++ server.py
@@ -9,14 +9,16 @@ app = Flask(__name__)
 @app.route("/")
 def login():
     # TODO change route to /login
+
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
-    # render the login template
-    return render_template('login.html')
+    # render the login template with the template variable state
+    return render_template('login.html', STATE=state)
 
 
 if __name__ == '__main__':
+    # TODO change secret_key
     app.secret_key = 'super secret key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
