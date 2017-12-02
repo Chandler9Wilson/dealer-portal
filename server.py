98fee388eae9911ede221e5b9f50223571d69119
Chandler Wilson <chandler9wilson@gmail.com>
2017-12-02 15:47:31 -0600

    added gconnect (not working though), gitignore added

--- server.py
+++ server.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template
+# TODO sort imports https://stackoverflow.com/a/20763446/6879253
+from flask import Flask, render_template, make_response
 from flask import session as login_session
 import random
 import string
+import json
+import httplib2
+import requests
+
+# TODO update to a different library oauth2client is depreciated
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+
+
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def login():
     # TODO change route to /login
 
@@ -15,6 +27,17 @@ def login():
     login_session['state'] = state
     # render the login template with the template variable state
     return render_template('login.html', STATE=state)
+
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response('Hello World!', 200)
+        return response
 
 
 if __name__ == '__main__':
