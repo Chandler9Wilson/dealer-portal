001beccdffaf2b3c796e96ab2c2e698e7ae9e885
Chandler Wilson <chandler9wilson@gmail.com>
2017-12-23 14:58:22 -0600

    database_mapper updated to include user tables

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 * update import organization
 * update variable names to follow [this](http://flask.pocoo.org/docs/0.12/styleguide/#naming-conventions)
 * remove debug prints
+* write documentation on dbManagment
--- dbManagment/database_mapper.py
+++ dbManagment/database_mapper.py
@@ -47,6 +47,7 @@ class Device(Base):
 
 
 class Data(Base):
+    # Reguraly collected data will be constantly hit with updates
 
     __tablename__ = 'data'
 
@@ -60,6 +61,45 @@ class Data(Base):
     fan_on = Column(Boolean, nullable=False)
     device_id = Column(Integer, ForeignKey('device.id'))
     device = relationship(Device)
+
+
+# Start user related tables
+
+
+class User(Base):
+    # A user of the website
+
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String, nullable=False)
+    profile_pic = Column(String)
+    oauth_provider = Column(String, nullable=False)
+
+
+class UserToFacility(Base):
+    # A workers relationship to a facility to be used with permissions
+
+    __tablename__ = 'user_to_facility'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    facility_id = Column(Integer, ForeignKey('facility.id'))
+    facility = relationship(Facility)
+
+
+class Role(Base):
+    # This design might need a revision currently
+    # a join will be needed everytime a role is pulled
+
+    __tablename__ = 'role'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(20), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 
 engine = create_engine('postgresql://catalog:catalog@localhost:5432/acmonitor')
