d7d93e51f923ead9696c6c78cdd3faf32aaeac16
Chandler Wilson <chandler9wilson@gmail.com>
2018-03-02 20:05:10 -0600

    Customer component now pulls data and displays it (ungracefully)

--- static/src/components/customer.vue
+++ static/src/components/customer.vue
@@ -1,9 +1,37 @@
 <template>
-  <div>This is Customer #{{ $route.params.id }}</div>
+  <div>
+    This is Customer #{{ $route.params.id }}
+    <div>My name is {{ name }}</div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'customer'
+  name: 'customer',
+  data() {
+    return {
+      id: null,
+      name: null,
+      loading: null
+    }
+  },
+  created() {
+    this.fetchCustomer()
+  },
+  methods: {
+    fetchCustomer() {
+      var self = this
+      self.loading = true
+      var uri = '/api/customers/' + this.$route.params.id
+
+      fetch(uri).then(function(response) {
+        return response.json()
+      }).then(function(customerJSON) {
+        self.id = customerJSON.id
+        self.name = customerJSON.name
+        self.loading = false
+      })
+    }
+  }
 }
 </script>
