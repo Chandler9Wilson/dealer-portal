a7d5b46630f113c7473db2e972fe731cd26fb3e6
Chandler Wilson <chandler9wilson@gmail.com>
2018-03-15 21:06:02 -0500

    Added customer styling, update, and delete

--- portal_server/directory/home_static/src/components/customer.vue
+++ portal_server/directory/home_static/src/components/customer.vue
@@ -1,7 +1,83 @@
 <template>
   <div>
-    This is Customer #{{ $route.params.id }}
-    <div>My name is {{ name }}</div>
+    <section class="hero is-info">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-two-thirds">
+              <h1 class="title">
+                Customer #{{ $route.params.id }}
+              </h1>
+              <h2 class="subtitle">
+                {{ name }}
+              </h2>
+            </div>
+            <div class="column is-one-third">
+              <a @click="deleteWarning=true" class="button is-danger is-pulled-right">Delete</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="spinner" v-if="loading">
+      <div class="rect1"></div>
+      <div class="rect2"></div>
+      <div class="rect3"></div>
+      <div class="rect4"></div>
+      <div class="rect5"></div>
+    </div>
+
+    <div v-if="error">
+      <!-- TODO improve error display -->
+      {{ error }}
+    </div>
+
+    <div v-if="name">
+    <!-- .prevent = the submit event will no longer reload the page -->
+      <form class="box" @submit.prevent="updateCustomer">
+        <div class="field">
+          <label class="label">Name</label>
+          <div class="control">
+            <input v-model="name" class="input" type="text" placeholder="Bowditch Navigation">
+          </div>
+        </div>
+
+        <div class="field is-grouped is-grouped-right">
+          <div class="control">
+            <button class="button is-primary">Save Changes</button>
+          </div>
+        </div>
+      </form>
+    </div>
+
+    <div v-if="deleteWarning">
+      <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+          <article class="message is-danger">
+            <div class="message-header">
+              <p>Warning</p>
+              <button @click="deleteWarning=null" class="delete" aria-label="delete"></button>
+            </div>
+            <div class="message-body">
+              <div class="columns is-centered">
+                <div class="column is-6">
+                  <p>This will <strong>permanently delete</strong> Customer #{{ $route.params.id }}</p>
+                </div>
+              </div>
+              <div class="columns">
+                <div class="column is-12">
+                  <a @click="deleteCustomer" class="button is-danger is-pulled-right">Delete</a>
+                  <a @click="deleteWarning=null" class="button is-pulled-right">Cancel</a>
+                </div>
+              </div>
+            </div>
+
+          </article>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,27 +86,104 @@ export default {
   name: 'customer',
   data() {
     return {
-      id: null,
       name: null,
-      loading: null
+      error: null,
+      loading: null,
+      deleteWarning: null
     }
   },
+  // Called after view creation
   created() {
     this.fetchCustomer()
   },
+  watch: {
+    '$route' (to, from) {
+      this.clearData()
+      this.fetchCustomer()
+    }
+  },
   methods: {
+    // TODO add error handling
     fetchCustomer() {
       var self = this
       self.loading = true
-      var uri = '/api/customers/' + this.$route.params.id
+      var url = '/api/customers/' + self.$route.params.id + '/'
 
-      fetch(uri).then(function(response) {
-        return response.json()
+      fetch(url).then(function(response) {
+        if (response.ok) {
+          return response.json()
+        }
+        // TODO improve this error
+        throw new Error('Network response was not ok.')
       }).then(function(customerJSON) {
-        self.id = customerJSON.id
         self.name = customerJSON.name
         self.loading = false
       })
+    },
+    updateCustomer() {
+      var self = this
+      self.loading = true
+      var url = '/api/customers/' + self.$route.params.id + '/'
+
+      var myInit = {
+        method: 'PUT',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        // https://developer.mozilla.org/en-US/docs/Web/API/Request/credentials
+        credentials: 'same-origin',
+        body: JSON.stringify({
+          name: self.name,
+        })
+      }
+      fetch(url, myInit).then(function(response) {
+        if (response.ok) {
+          return response.json()
+        }
+        // TODO improve this error
+        throw new Error('Network response was not ok.')
+      }).then(function(customerJSON) {
+        self.clearData()
+
+        self.name = customerJSON.name
+        self.loading = false
+      })
+    },
+    deleteCustomer() {
+      var self = this
+      self.loading = true
+      self.deleteWarning = null
+      var url = '/api/customers/' + self.$route.params.id + '/'
+
+      var myInit = {
+        method: 'DELETE',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        // https://developer.mozilla.org/en-US/docs/Web/API/Request/credentials
+        credentials: 'same-origin',
+      }
+      fetch(url, myInit).then(function(response) {
+        if (response.status == 204) {
+          return null
+        }
+        // TODO improve this error
+        throw new Error('Network response was not ok.')
+      }).then(function() {
+        self.clearData()
+        self.$router.push('/')
+      })
+    },
+    // TODO make this clear dynamically
+    clearData() {
+      var self = this
+
+      self.name = null
+      self.error = null
+      self.loading = null
+      self.deleteWarning = null
     }
   }
 }
