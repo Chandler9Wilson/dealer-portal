ee1c1bc10b002d48fda2be24186abd75282f0468
Chandler Wilson <chandler9wilson@gmail.com>
2018-03-16 14:48:52 -0500

    Added icon support through fontawesome

--- portal_server/directory/home_static/src/js/directory.js
+++ portal_server/directory/home_static/src/js/directory.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import router from '../router'
 import App from './../App.vue'
 
+import fontawesome from '@fortawesome/fontawesome'
+import faCheck from '@fortawesome/fontawesome-free-solid/faCheck'
+
 require('bulma/css/bulma.css')
+
+fontawesome.library.add(faCheck)
 
 /* eslint-disable no-new */
 new Vue({
