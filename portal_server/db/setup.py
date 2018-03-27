1033b548279d3e4dfa7494a23f1b5a6b71d55e87
Chandler Wilson <chandler9wilson@gmail.com>
2018-03-27 17:35:21 -0500

    Updated imports for package use

--- portal_server/db/setup.py
+++ portal_server/db/setup.py
@@ -2,8 +2,8 @@
 # Documentation on why above is needed \
 # https://stackoverflow.com/a/28154841/6879253
 
-from db.models import db
-from run import app
+from portal_server import db
+from portal_server import app
 
 with app.test_request_context():
     db.init_app(app)
