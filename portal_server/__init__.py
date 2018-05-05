8af9176c312d7a58706027df9fe0d761ad508062
Chandler Wilson <chandler9wilson@gmail.com>
2018-05-05 13:27:07 -0500

    Removed Caching prevention

--- portal_server/__init__.py
+++ portal_server/__init__.py
@@ -50,25 +50,6 @@ app.register_blueprint(login_bp)
 db.init_app(app)
 
 
-@app.context_processor
-# TODO remove before deployment
-def override_url_for():
-    """Generate a new token on every request to prevent the browser from
-    caching static files.
-    """
-    return dict(url_for=dated_url_for)
-
-
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(app.root_path,
-                                     endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
-
-
 @app.errorhandler(404)
 def not_found(error):
     # Handle 404 errors so that they make more sense for the api
