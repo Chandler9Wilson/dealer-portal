44ac30a345b5f6bfa0ace990fbaf49278a8e6c1d
Chandler Wilson <chandler9wilson@gmail.com>
2018-05-07 18:48:39 -0500

    Added flask principal, Added admin and admin_api blueprints, Added docstring on `load_user`

--- portal_server/__init__.py
+++ portal_server/__init__.py
@@ -3,7 +3,10 @@ import os
 from flask import Flask, url_for, redirect
 
 # flask-login used for login management and persistence
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user
+
+# flask-principal is used for per resource control and permissions
+from flask_principal import Principal, identity_loaded, RoleNeed, UserNeed
 
 # Import database classes and SQLAlchamy instance
 from portal_server.db.models import db, User
@@ -12,6 +15,8 @@ from portal_server.db.models import db, User
 from portal_server.api.endpoints import api
 from portal_server.login_management.login import login_bp
 from portal_server.directory.home import directory
+from portal_server.admin.dashboard import admin
+from portal_server.admin.management import admin_api
 
 config = {
     'development': 'portal_server.config.DevelopmentConfig',
@@ -35,15 +40,37 @@ login_manager = LoginManager()
 login_manager.login_view = 'login_bp.login'
 login_manager.init_app(app)
 
+# Flask-principal load
+principals = Principal(app)
+
 
 @login_manager.user_loader
 def load_user(id):
+    """This is a user loader used by flask-login"""
     return User.query.filter_by(id=id).first()
+
+
+@identity_loaded.connect_via(app)
+def on_identity_loaded(sender, identity):
+    # Set the identity user object
+    identity.user = current_user
+
+    # Add the UserNeed to the identity
+    if hasattr(current_user, 'id'):
+        identity.provides.add(UserNeed(current_user.id))
+
+    # Assuming the User model has a list of roles, update the
+    # identity with the roles that the user provides
+    if hasattr(current_user, 'roles'):
+        for role in current_user.roles:
+            identity.provides.add(RoleNeed(role.title))
 
 
 # Register blueprints
 app.register_blueprint(api, url_prefix='/api')
 app.register_blueprint(directory, url_prefix='/home')
+app.register_blueprint(admin, url_prefix='/admin')
+app.register_blueprint(admin_api, url_prefix='/admin/api')
 app.register_blueprint(login_bp)
 
 # Starts up flask-sqlalchemy
