7ae91e15d91a715d9c553e2b8c2cc77542fc4795
Chandler Wilson <chandler9wilson@gmail.com>
2018-05-27 22:01:02 -0500

    Updated to serve static content not a template

--- portal_server/directory/home.py
+++ portal_server/directory/home.py
@@ -1,14 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, send_from_directory
 
 # flask-login used for login management and persistence
 from flask_login import login_required
 
 directory = Blueprint('directory', __name__,
-                      static_folder='home_static',
+                      static_folder='home_static/dist',
                       template_folder='templates')
 
 
-@directory.route('/')
+@directory.route('/', methods=['GET'])
+@directory.route('/<path:filename>', methods=['GET'])
 @login_required
-def home():
-    return render_template('directory.html')
+def home(filename='index.html'):
+    return send_from_directory(directory.static_folder, filename)
