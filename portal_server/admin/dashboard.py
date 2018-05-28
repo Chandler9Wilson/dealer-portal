f01293f724401016bc5aaf95a99d9d53b4ed1a80
Chandler Wilson <chandler9wilson@gmail.com>
2018-05-27 19:46:14 -0500

    Now served admin static assets correctly

--- portal_server/admin/dashboard.py
+++ portal_server/admin/dashboard.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, send_from_directory
 
 # flask-login used for login management and persistence
 from flask_login import current_user
@@ -8,13 +8,14 @@ from flask_principal import Principal, Permission, RoleNeed
 
 admin_role = Permission(RoleNeed('admin'))
 
-admin = Blueprint('admin', __name__)
+admin = Blueprint('admin', __name__, static_folder='admin_static/dist')
 
 
 @admin.route('/', methods=['GET'])
+@admin.route('/<path:filename>', methods=['GET'])
 @admin_role.require(http_exception=403)
-def serve_admin_dashboard():
-    return 'This is the dashboard'
+def serve_admin_dashboard(filename='index.html'):
+    return send_from_directory(admin.static_folder, filename)
 
 
 @admin.errorhandler(403)
